ac990a36ea4b336730f570f45a10f7726c37db8a
gotarazo <2272@holbertonschool.com>
2020-09-17 03:43:12 +0000

    C programming printf commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Print mesagge
+* Return: 0
+* Description: This program prints a text using printf
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
