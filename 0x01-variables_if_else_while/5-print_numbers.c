7e070db08432f22f93783e268df03c07b600f5a6
gotarazo <2272@holbertonschool.com>
2020-09-18 04:00:25 +0000

    C if else print digits base 10 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main- for that prints the digits in base 10
+* Description: using only the putchar function
+* Return: 0
+*/
+
+int main(void)
+{
+char digit;
+for (digit = '0'; digit <= '9'; digit++)
+putchar(digit);
+putchar('\n');
+return (0);
+}
