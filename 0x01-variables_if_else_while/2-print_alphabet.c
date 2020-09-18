75c6885194fee66d864e581fffae038f3c484d0d
gotarazo <2272@holbertonschool.com>
2020-09-18 03:00:46 +0000

    C if else printf alphabet commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main- for that prints the alphabet in lowercase
+* Description: using only the putchar function
+* Return: 0
+*/
+
+int main(void)
+{
+char alpha;
+for (alpha = "a"; alpha <= "z"; alpha++)
+putchar(alpha);
+putchar("\n");
+return (0);
+}
