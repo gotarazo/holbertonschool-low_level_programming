e54f85615ee238498e9b37017b43d38557def7c7
gotarazo <2272@holbertonschool.com>
2020-09-22 02:55:27 +0000

    C Functions print alpha commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * -main for loop prints the alphabet, in lowercase
+ * Return: 0
+ */
+
+int print_alphabet(void)
+{
+char alpha = 'a';
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+_putchar(alpha);
+_putchar('\n');
+return (0);
+}
