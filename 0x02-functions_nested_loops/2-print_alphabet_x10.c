00c323d47add5740df8a79e650a616a44e39b600
gotarazo <2272@holbertonschool.com>
2020-09-22 03:38:46 +0000

    C Functions print 10 alpha commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* print_alphabet_x10 -For loop prints the alphabet, in lowercase
+* Description: Function use _putchar twice
+* Return: 0
+*/
+
+int print_alphabet_x10(void)
+{
+int veces;
+char alpha;
+for (veces = 1; veces <= 10; veces++)
+{
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+_putchar(alpha);
+_putchar('\n');
+}
+return (0);
+}
