4d9d4240a312b63a49b04d07b99e8235f7b4fa7f
gotarazo <2272@holbertonschool.com>
2020-09-23 01:46:10 +0000

    C functions add 2 integers commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* add -function that adds two integers and returns the result
+* @num1: First Number to add
+* @num2: Second Number to add
+* Return: Result of sum
+*/
+
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
+
