46192415c2a547d01c27532125dc7c6916b5983f
gotarazo <2272@holbertonschool.com>
2020-09-24 22:53:06 +0000

    C more functions print numbers commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* print_most_numbers- Prints the numbers, from 0 to 9
+* Return: Numbers
+*/
+
+void print_most_numbers(void)
+{
+char i;
+for (i = '0'; i <= '9'; i++)
+{
+if (i != '2' || i != '4')
+_putchar(i);
+}
+_putchar('\n');
+}
