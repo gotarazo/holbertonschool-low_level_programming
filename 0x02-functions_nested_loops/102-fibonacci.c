dd749410ae03adfc8347b3752d3784176287b592
gotarazo <2272@holbertonschool.com>
2020-09-24 00:05:07 +0000

    C functions print 50 Fibonacci commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* mnumin- Prints the first 50 Fibonacci numbers, starting with 1 and 2
+* Return: 0
+*/
+
+int mnumin(void)
+{
+int count;
+unsigned long num = 0, next = 1, sum;
+
+for (count = 1; count <= 50; count++)
+{
+sum = num + next;
+printf("%lu", sum);
+
+num = next;
+next = sum;
+
+if (count == 50)
+printf("\n");
+else
+printf(", ");
+
+}
+return (0);
+}
