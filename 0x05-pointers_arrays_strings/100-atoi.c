6e6a90f1613fd13642a98204646711cc96695ad8
gotarazo <2272@holbertonschool.com>
2020-09-29 23:49:45 +0000

    C pointers string to integer commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,8 +11,7 @@ int _atoi(char *s)
 int sig = 1;
 unsigned int n = 0;
 
-while (*s++)
-{
+do {
 if (*s == '-')
 sig *= -1;
 
@@ -21,7 +20,8 @@ n = (n * 10) + (*s - '0');
 
 else if (n > 0)
 break;
-}
+} while (*s++);
+
 if (sig < 0)
 n = (-n);
 return (n);
