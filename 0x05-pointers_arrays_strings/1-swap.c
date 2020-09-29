ca34c163f0498dd584f713156805d1f5964bd0ee
gotarazo <2272@holbertonschool.com>
2020-09-29 05:15:32 +0000

    C pointers swap 2 integers commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
 /**
- * swap_int- Swaps the values of two integers
- * @a: First integer to be swapped
- * @b: Second integer to be swapped
- */
+* swap_int- Swaps the values of two integers
+* @a: First integer to be swapped
+* @b: Second integer to be swapped
+*/
 
 void swap_int(int *a, int *b)
 {
-  int swap = *a;
-  *a = *b;
-  *b = swap;
+int swap = *a;
+*a = *b;
+*b = swap;
 }
