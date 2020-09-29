48df5f52ca35c35e28f437dfa24da785d402b372
gotarazo <2272@holbertonschool.com>
2020-09-29 20:03:51 +0000

    C pointers half string commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,17 +7,17 @@
 
 void puts_half(char *str)
 {
-int point = 0, lenght = 0, n;
+int point = 0, length = 0, n;
 
 while (str[point++])
-lenght++;
+length++;
 
-if ((len % 2) == 0)
-n = lenght / 2;
+if ((length % 2) == 0)
+n = length / 2;
 else
-n = (lenght + 1) / 2;
+n = (length + 1) / 2;
 
-for (point = n; point < lenght; point++)
+for (point = n; point < length; point++)
 _putchar(str[point]);
 
 _putchar('\n');
