d731f5c6ec0c1dd0b4ac5bdfee4295463215bd7d
gotarazo <2272@holbertonschool.com>
2020-10-01 22:21:42 +0000

    C more pointers concatenate 2 strings n bytes commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* _strcat- Concatenates two strings
+* @dest: String 1
+* @src: String 2
+* @n: Bytes from src
+* Return: Dest pointer with the resulting string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+int dest1, src1;
+
+for (dest1 = 0; dest[dest1] != '\0'; dest1++)
+;
+for (src1 = 0; src[src1] != '\0' && src1 < n; src1++)
+{
+dest[dest1 + src1] =  src[src1];
+}
+return (dest);
+}
