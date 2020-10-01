b1bcc4190f52c28fb31829d30012d388f235c249
gotarazo <2272@holbertonschool.com>
2020-10-01 23:06:57 +0000

    C more pointers compares 2 strings commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* _strcmp- compares 2 strings
+* @s1: String 1
+* @s2: String 2
+* Return: Difference between strings
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+int count;
+
+for (count = 0; (s1[count] == s2[count]) && (s1[count] != '\0'); count++)
+;
+return (*s1 - *s2);
+}
