4a304ba53e8703e878d8cb2dbf279abb83e70324
gotarazo <2272@holbertonschool.com>
2020-10-02 01:27:33 +0000

    C more pointers encodes string commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,11 +11,11 @@ char *leet(char *s)
 int str, let;
 int 1337[] = {'4', '3', '0', '7', '1', 'a', 'e', 'o', 'l', 't'};
 
-for (str = 0; s[str] != '\0'; str++)
+for (str = 0; s[str] != 0; str++)
 {
-for (let = 5; 1337[let] != '\0'; let++)
+for (let = 5; 1337[let] != 0; let++)
 {
-if (s[str] == 1337[let] || s[str] == (1337[let] - ' '))
+if (s[str] == 1337[let] || s[str] == (1337[let] - 32))
 {
 s[str] = 1337[let - 5];
 }
