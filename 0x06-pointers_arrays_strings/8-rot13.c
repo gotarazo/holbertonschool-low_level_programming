f483103a67e0988e378a84b12595d7f069835f0b
gotarazo <2272@holbertonschool.com>
2020-10-02 03:07:20 +0000

    C more pointers encode rot13 commit

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+* rot13- Encodes a string using rot13
+* @s: String
+* Return: String encode
+*/
+
+char *rot13(char *s)
+{
+int count1, count2;
+char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char cod[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+for (count1 = 0; s[count1] != 0; count1++)
+{
+for (count2 = 0; alpha[count2] != 0; count2++)
+{
+if (s[count1] == alpha[count2])
+s[count1] = cod[count2];
+break;
+}
+}
+return (s);
+}
