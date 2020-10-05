0321d42b8db073fe6bfd8739c200aad2544eacd0
gotarazo <2272@holbertonschool.com>
2020-10-05 16:26:16 +0000

    C even pointers search string commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* _strpbrk- Searches a string for any of a set of bytes
+* @s: String for search
+* @accept: String of bytes to search
+* Return: Pointer to byte in s that matches one of bytes in accept (or NULL)
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+while (*s)
+{
+if (_strchr(accept, *s++))
+return (--s);
+}
+return (0);
+}
