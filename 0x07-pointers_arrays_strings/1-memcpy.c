00901fe1c3749e1efbd379fc286dc986c09f7d74
gotarazo <2272@holbertonschool.com>
2020-10-05 15:36:06 +0000

    C even pointers copy memory commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* _memcpy- Copies memory area
+* @src: Source memory area
+* @dest: Destination memory area
+* @n: Number of bytes from memory area
+* Return: Modified array.
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int point;
+
+for (point = 0; point < n; point++)
+dest[i] = src[i];
+
+return (dest);
+}
