5541e3c4d357a72ab1c7c77ac4fe6771378da4b8
gotarazo <2272@holbertonschool.com>
2020-10-05 23:30:59 +0000

    C even pointers search string commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,7 @@
 * _strpbrk- Searches a string for any of a set of bytes
 * @s: String for search
 * @accept: String of bytes to search
-* Return: Pointer to byte in s that matches one of bytes in accept
+* Return: Pointer to byte in s that matches one of bytes in accept (or NULL)
 */
 
 char *_strpbrk(char *s, char *accept)
@@ -13,9 +13,7 @@ char *_strpbrk(char *s, char *accept)
 while (*s)
 {
 if (_strchr(accept, *s++))
-{
 return (s);
 }
-}
-return (0);
+return ('\0');
 }
