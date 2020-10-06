40bfb27bbfd16502597556e54e33b910b77311d5
gotarazo <2272@holbertonschool.com>
2020-10-06 01:01:29 +0000

    C even pointers locates substring string commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+* _strstr- Locates a substring finds the first occurrence
+* @haystack: Source string
+* @needle: String to look
+* Return: Pointer to the beginning of the located substring
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+unsigned int coun1, count2;
+
+if (needle[0] == '\0')
+return (haystack);
+
+for (count1 = 0; haystack[count1] != '\0'; count1++)
+{
+for (count2 = 0; needle[count2] == haystack[count1 + count2]; count2++)
+{
+if (needle[count2 + 1] == '\0')
+return (haystack + count1);
+}
+}
+return (0);
+}
