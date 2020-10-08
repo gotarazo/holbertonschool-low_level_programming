af0b718c6148bcf501455eb5bc92a3adaa1d3dfa
gotarazo <2272@holbertonschool.com>
2020-10-08 03:34:57 +0000

    C recursion square root commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+* sqrt- Square root of n
+* @n: Number
+* @m: Posible square
+* Return: Square (-1 If n does not have a natural square root)
+*/
+
+int sqrt(int n, int m)
+{
+if (m * m < n)
+m = sqrt(n, m + 1);
+
+if (m * m > n)
+return (-1);
+
+return (m);
+}
+
+/**
+* _sqrt_recursion- Returns the natural square root of a number
+* @n: Number
+* Return: Square root
+*/
+
+int _sqrt_recursion(int n)
+{
+return (sqrt(n, 1));
+}
