d09c8159c3a9efa859db4a3f9e2d5d7d30616c8d
gotarazo <2272@holbertonschool.com>
2020-10-12 22:36:33 +0000

    C argc prints number arguments commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main- Prints the number of arguments passed into it
+* @argc: Number of arguments
+* @argv: Array of arguments
+* Return: 0
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
