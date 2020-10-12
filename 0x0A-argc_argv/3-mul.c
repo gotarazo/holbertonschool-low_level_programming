45b3a91c4d65ee646c22badb24b38e0de2f04166
gotarazo <2272@holbertonschool.com>
2020-10-12 22:57:15 +0000

    C argc multiplies 2 numbers commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
 * main- Multiplies two numbers
 * @argc: Number 1
 * @argv: Number 2
-* Return: 0
+* Return: 0 or 1 (if error)
 */
 
 int main(int argc, char *argv[])
 {
 if (argc == 3)
-{
 printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
-}
+
 else
-{
 printf("Error\n");
 return (1);
-}
 }
