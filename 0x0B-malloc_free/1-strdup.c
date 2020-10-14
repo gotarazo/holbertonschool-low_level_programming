bb8f95461ea96a87ad9a83c2769b64328b6fe751
gotarazo <2272@holbertonschool.com>
2020-10-14 02:47:32 +0000

    C malloc copy string  commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* *_strdup- returns a pointer to a newly allocated space in memory
+* which contains a copy of the string given as a parameter
+* @str: String
+* Return: Pointer to the duplicated string or NULL (if str NULL)
+*/
+
+char *_strdup(char *str)
+{
+char *c;
+int i, n;
+
+if (str == NULL)
+return (NULL);
+for (i = 0; str[n] != '\0'; i++)
+;
+i++;
+c = malloc(n * sizeof(*c));
+if (c == NULL)
+return (NULL);
+for (n = 0; n < i; n++)
+c[n] = str[n];
+return (c);
+}
