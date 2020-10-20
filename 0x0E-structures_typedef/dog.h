89d8dfb657620d00747095e34e31a6133797ad8c
gotarazo <2272@holbertonschool.com>
2020-10-20 03:19:35 +0000

    C structures dog commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#defineDOG_H
+
+/**
+* struct dog- Dogs structure
+* @name: Dog's name
+* @age: Dog's age
+* @owner: Owner's name
+*/
+
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
