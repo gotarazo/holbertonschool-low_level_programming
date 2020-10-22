4f940d4e05bfd2dce18191d63a7e51399913e36d
gotarazo <2272@holbertonschool.com>
2020-10-22 04:50:06 +0000

    Function pointers calc commit

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+* op_add- Sum 2 numbers
+* @a: Number 1
+* @b: Number 2
+* Return: Result
+*/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* op_sub- Subtract 2 numbers
+* @a: Number 1
+* @b: Number 2
+* Return: Result
+*/
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+* op_mul- Multiplay 2 numbers
+* @a: Number 1
+* @b: Number 2
+* Return: Prroduct
+*/
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+* op_div- Divide 2 numbers
+* @a: Number 1
+* @b: Number 2
+* Return: Division
+*/
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+* op_mod- Modulate 2 numbers
+* @a: Number 1
+* @b: Number 2
+* Return: Module
+*/
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
