e0ed9fe371c7ff228960b42041a4fae2d79d599e
gotarazo <2272@holbertonschool.com>
2020-10-22 04:58:00 +0000

    Function pointers calc commit

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+* *get_op_func- contain the function that selects the correct function to
+* perform the operation asked by the user
+* @s: Operator passed as argument to the program
+* Return: Integer or NULL (not match any of the 5 expected operators)
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
+
+i = 0;
+while (i < 5)
+{
+if (*ops[i].op == *s)
+return (ops[i].f);
+
+i++;
+}
+return (NULL);
+}
