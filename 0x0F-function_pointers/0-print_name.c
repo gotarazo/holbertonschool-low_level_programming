65a2a679af75d559d46ce5b1da6ed2a99bbf11d5
gotarazo <2272@holbertonschool.com>
2020-10-22 02:37:56 +0000

    Function pointers print name commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* print_name- Prints a name
+* @name: String
+* @f: Funtion pointer
+* Return: Name printed
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+f(name);
+}
