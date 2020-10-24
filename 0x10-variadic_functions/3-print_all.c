dce6e2553c16333402f971d4770a0440bf6f98a0
gotarazo <2272@holbertonschool.com>
2020-10-24 19:36:00 +0000

    Variadic functions print all commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,66 +1,4 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
-
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
-void print_all(const char * const format, ...);
-
-/**
-* print_char- Prints a char
-* @arg: Arguments pointing to the character
-*/
-
-void print_char(va_list arg)
-{
-char ch;
-ch = va_arg(arg, int);
-printf("%c", ch);
-}
-
-/**
-* print_int- Prints an integer
-* @arg: Arguments pointing to the integer
-*/
-
-void print_int(va_list arg)
-{
-int n;
-n = va_arg(arg, int);
-printf("%d", n);
-}
-
-/**
-* print_float- Prints a float
-* @arg: Arguments pointing to the float
-*/
-
-void print_float(va_list arg)
-{
-float flot;
-flot = va_arg(arg, double);
-printf("%f", flot);
-}
-
-/**
-* print_string- Prints a string
-* @arg: Arguments pointing to the string
-*/
-
-void print_string(va_list arg)
-{
-char *str;
-str = va_arg(arg, char *);
-
-if (str == NULL)
-{
-printf("(nil)");
-return;
-}
-printf("%s", str);
-}
 
 /**
 * print_all- Prints anything (followed by a new line)
@@ -71,30 +9,41 @@ printf("%s", str);
 void print_all(const char * const format, ...)
 {
 va_list args;
-int i = 0, j = 0;
-char *separator = "";
+unsigned int i = 0, j = 1;
+char *str;
 
-PrintFormat_t fs[] = {
-{"c", print_char},{"i", print_int},{"f", print_float},{"s", print_string}
-};
-
+while (format)
+{
 va_start(args, format);
-
-while (format && (*(format + i)))
+while (format[i])
 {
+switch (format[i])
+{
+case 'c':
+printf("%c", va_arg(args, int));
+break;
+case 'i':
+printf("%d", va_arg(args, int));
+break;
+case 'f':
+printf("%f", va_arg(args, double));
+break;
+case 's':
+str = va_arg(args, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s", str);
+break;
+default:
 j = 0;
-
-while (j < 4 && (*(format + i) != *(fs[j].form)))
-j++;
-
-if (j < 4)
-{
-printf("%s", separator);
-fs[j].prnt(args);
-separator = ", ";
+break;
 }
+if (format[i + 1] && j)
+printf(", ");
 i++;
 }
-printf("\n");
 va_end(args);
+break;
+}
+printf("\n");
 }
