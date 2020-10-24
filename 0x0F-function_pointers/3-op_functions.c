11544572d8379435cb999f788db1318e5b06f38b
gotarazo <2272@holbertonschool.com>
2020-10-24 07:33:16 +0000

    Function pointers calc commit

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -45,6 +45,11 @@ return (a * b);
 
 int op_div(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a / b);
 }
 
@@ -57,5 +62,10 @@ return (a / b);
 
 int op_mod(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a % b);
 }
