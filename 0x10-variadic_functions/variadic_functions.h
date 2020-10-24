efd2b94efa2a75187a28500c361a28bfd9ffd87c
gotarazo <2272@holbertonschool.com>
2020-10-24 20:19:19 +0000

    Variadic functions print all commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,21 +2,9 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
 
-/**
-* struct PrintFormat- Function that matches the @format
-* @format: Format that argument should be printed
-* @function: Function associated with @format
-*/
-
-typedef struct PrintFormat
-{
-char *form;
-void (*prnt)(va_list);
-} PrintFormat_t;
-
-int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
