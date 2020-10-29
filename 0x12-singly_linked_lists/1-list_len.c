e48381271bb62d8d3210b4d2d79c28d14955f4ca
gotarazo <2272@holbertonschool.com>
2020-10-29 23:51:40 +0000

    C linked lists elements number commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* list_len- Returns the number of elements in a linked list
+* @h: List
+* Return: Number of elements in h
+*/
+
+size_t list_len(const list_t *h)
+{
+size_t n = 0;
+
+while (h)
+{
+elements++;
+h = h->next;
+}
+return (n);
+}
