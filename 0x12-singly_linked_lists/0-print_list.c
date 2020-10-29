7a7539822ba464ac771f8665ac144c7ae2bfebe8
gotarazo <2272@holbertonschool.com>
2020-10-29 23:31:45 +0000

    C linked lists commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* print_list- Prints all the elements of a list
+* @h: List
+* Return: Number of nodes in h
+*/
+
+size_t print_list(const list_t *h)
+{
+size_t n = 0;
+
+while (h)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+
+else
+printf("[%d] %s\n", h->len, h->str);
+
+n++;
+h = h->next;
+}
+return (n);
+}
