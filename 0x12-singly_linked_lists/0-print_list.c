943f949e1f5b0196cceb37b100388731957a676c
gotarazo <2272@holbertonschool.com>
2020-10-30 05:49:12 +0000

    C linked lists print list commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
 * print_list- Prints all the elements of a list
@@ -9,9 +10,6 @@
 size_t print_list(const list_t *h)
 {
 size_t n = 0;
-
-if (h == NULL)
-return (0);
 
 while (h)
 {
