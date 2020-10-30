a95ddd590c72a100dbfba1654c712c58edaa0775
gotarazo <2272@holbertonschool.com>
2020-10-30 04:07:51 +0000

    C linked lists first commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+* print_first- Prints a string before the main function is executed
+* Return: Void
+*/
+
+__attribute__ ((constructor))
+void print_first(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+}
