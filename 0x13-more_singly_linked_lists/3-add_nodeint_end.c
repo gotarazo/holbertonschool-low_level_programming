6b69d852be7b53bb14c0cbb4f187279d8b61457e
gotarazo <2272@holbertonschool.com>
2020-11-04 05:11:19 +0000

    C linked lists add nodeint end commit

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+* add_nodeint_end- Adds a new node at the end of a list
+* @head: Double pointer to head of list
+* @n: Integer (data to put in the new node)
+* Return: new, the address of the new element or NULL (if it failed)
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+listint_t *aux = NULL, *new = NULL;
+aux = *head;
+
+if (aux)
+{
+while (aux->next)
+aux = aux->next;
+}
+new = malloc(sizeof(listint_t));
+if (!new)
+return (NULL);
+new->n = n;
+new->next = NULL;
+if (aux)
+aux->next = new;
+else
+*head = new;
+return (new);
+}
