b80999c66467aa5d67ac9ae2126328f1cb7fa0a5
gotarazo <2272@holbertonschool.com>
2020-11-04 01:02:21 +0000

    C linked lists add nodeint commit

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* add_nodeint- Adds a new node at the beginning of a list
+* @head: Pointer to the address of the head of the list
+* @n: Integer for the new node
+* Return: The address of the new element or NULL (if it failed)
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *aux;
+
+aux = malloc(sizeof(listint_t));
+if (aux == NULL)
+return (NULL);
+
+aux->n = n;
+aux->next = *head;
+
+*head = aux;
+
+return (new);
+}
