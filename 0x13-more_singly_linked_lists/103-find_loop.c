0bffeb90cf9e01cf8cfa1d01441cd15b4c76042f
gotarazo <2272@holbertonschool.com>
2020-11-05 04:50:04 +0000

    C linked lists find loop commit

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* listint_t *find_listint_loop- Finds the loop in a linked list
+* @head: Pointer o head of list
+* Return: adv1, address of node where the loop starts or NULL if no loop
+*/
+
+
+
+listint_t *find_listint_loop(listint_t *head)
+{
+listint_t *adv1 = head, *adv2 = head;
+
+while (adv1 && adv2 && adv2->next)
+{
+adv1 = adv1->next;
+adv2 = adv2->next->next;
+if (adv1 == adv2)
+return (adv1);
+}
+return (NULL);
+}
