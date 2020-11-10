f861734a04f86515c2ebf616414fd423279a235c
gotarazo <2272@holbertonschool.com>
2020-11-10 04:52:30 +0000

    C File I/O copy to file commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,27 +13,27 @@ char buff[1024];
 int from, to, len;
 
 if (ac != 3)
-ERROR1(97, "Usage: cp from to");
+er1(97, "Usage: cp from to");
 
 from = open(av[1], O_RDONLY);
 if (from == -1)
-ERROR2(98, "Error: Can't read from file", av[1]);
+er2(98, "Error: Can't read from file", av[1]);
 
 to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
 if (to == -1)
-ERROR2(99, "Error: Can't write to", av[2]);
+er2(99, "Error: Can't write to", av[2]);
 
 while ((len = read(from, buff, 1024)) > 0)
 {
 if ((write(to, buff, len)) == -1)
-ERROR2(99, "Error: Can't write to", av[2]);
+er2(99, "Error: Can't write to", av[2]);
 }
 if (len == -1)
-ERROR2(98, "Error: Can't read from file", av[1]);
+er2(98, "Error: Can't read from file", av[1]);
 if (close(from) == -1)
-ERROR3(100, "Error: Can't close fd", from);
+er3(100, "Error: Can't close fd", from);
 if (close(to) == -1)
-ERROR3(100, "Error: Can't close fd", to);
+er3(100, "Error: Can't close fd", to);
 
 return (0);
 }
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -8,6 +8,24 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#define er1(x, s1)\
+do {\
+dprintf(STDERR_FILENO, "%s\n", s1);\
+exit(x);\
+} while (0)
+
+#define er2(x, s1, s2)\
+do {\
+dprintf(STDERR_FILENO, "%s %s\n", s1, s2);\
+exit(x);\
+} while (0)
+
+#define er3(x, s1, fd)\
+do {\
+dprintf(STDERR_FILENO, "%s %d\n", s1, fd);\
+exit(x);\
+} while (0)
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
