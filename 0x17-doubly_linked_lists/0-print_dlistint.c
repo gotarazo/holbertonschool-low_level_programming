72a86e2c5097d796827eddebb29b49b211af43dd
gotarazo <2272@holbertonschool.com>
2020-12-11 16:56:03 +0000

    Doubly linked lists commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* print_dlistint- Prints all the elements of a dlistint_t list
+* @h: Head of the dlistint_t list
+* Return: nod (number of nodes)
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t nod = 0;
+
+while (h)
+{
+nod++;
+printf("%d\n", h->n);
+h = h->next;
+}
+return (nod);
+}
+x
