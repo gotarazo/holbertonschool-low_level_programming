50be577a341f1bdd52727ad99a5ba505894d6d47
gotarazo <2272@holbertonschool.com>
2021-01-15 00:30:23 +0000

    C hash tables commit

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,50 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_set - Add or update an element in a hash table.
+* @ht: Pointer to the hash table
+* @key: Key to add
+* @value: Value associated with the key
+* Return: 1 (if it succeeded) or 0 (in Otherwise)
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+hash_node_t *new;
+char *v_copy;
+unsigned long int index, i;
+
+if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+return (0);
+
+v_copy = strdup(value);
+if (v_copy == NULL)
+return (0);
+
+index = key_index((const unsigned char *)key, ht->size);
+for (i = index; ht->array[i]; i++)
+{
+if (strcmp(ht->array[i]->key, key) == 0)
+{
+free(ht->array[i]->value);
+ht->array[i]->value = v_copy;
+return (1);
+}
+}
+new = malloc(sizeof(hash_node_t));
+if (new == NULL)
+{
+free(v_copy);
+return (0);
+}
+new->key = strdup(key);
+if (new->key == NULL)
+{
+free(new);
+return (0);
+}
+new->value = v_copy;
+new->next = ht->array[index];
+ht->array[index] = new;
+return (1);
+}
